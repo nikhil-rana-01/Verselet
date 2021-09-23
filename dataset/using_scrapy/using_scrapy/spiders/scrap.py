7f499428f3ec8f6c69670a918951c137dd4e08ae
nikhil-rana-01 <nikhilrana515@gmail.com>
2021-09-23 11:04:39 +0530

    Update scrap.py

--- dataset/using_scrapy/using_scrapy/spiders/scrap.py
+++ dataset/using_scrapy/using_scrapy/spiders/scrap.py
@@ -2,20 +2,25 @@ import csv
 import scrapy
 
 class Get_Poems(scrapy.Spider):
+	# Name of the spider used for scraping data
 	name = "poems"
+	#different eras divided into from parts accordind to hindi literature
 	durations = [ [1050,1375] , [1375,1700], [1700,1900] , [1900, 2021]  ]
 	author_name_to_era = {}
-
+	#url of site from where datawill be extracted
 	start_urls = ["http://kavitakosh.org/kk/index.php"]
 	def parse(self, response):
 		authors = response.css("div.poet-list-section")
+		#this will get all the author links from the site using its html code (div and hrefs)
 		author_links = response.css('div.poet-list-section a::attr(href)').getall()
 		for author_link in author_links:
 			complete_link = "http://kavitakosh.org" + author_link
 			yield scrapy.Request(complete_link, self.parse_authors)
 
 	def parse_authors(self, response):
+		#this will check whether the given author extracted above information exist or not
 		does_author_info_exist = response.css('div.kkparichay-box').get()
+		#below we will get all the onformation of author with its name date and all
 		if does_author_info_exist is not None:
 			author_info = response.css('div.kkparichay-box li a::attr(href)').getall()
 			author_name = response.css('span#kkparichay-name::text').get()
@@ -23,7 +28,7 @@ class Get_Poems(scrapy.Spider):
 			author_name = author_name.strip()
 			self.author_name_to_era[author_name] = author_date
 
-			
+			#this will parse the links of poems pages written by that author
 			poem_of_author_links = response.css('li a::attr(href)').getall()
 			for poem_link in poem_of_author_links:
 				complete_link = "http://kavitakosh.org" + poem_link
@@ -31,6 +36,7 @@ class Get_Poems(scrapy.Spider):
 
 	def parse_poems(self, response):
 		author_poem = response.css('h1.firstHeading span::text').get()
+		#here split function will split author name and poem name as given below
 		if( author_poem is not None ):
 			author_poem = author_poem.split('/', 2)
 
@@ -40,11 +46,12 @@ class Get_Poems(scrapy.Spider):
 				
 				author_name = author_name.strip()
 				poem_name = poem_name.strip()
-
+				#in this file all the authors with there poem name are saved even if there poem exist or not
 				with open("all.txt", 'a') as file:
 					file.write(poem_name + "~" + author_name + "\n")
 				
 				poem_exist = response.css('div.poem').get()
+				#below only those author names and poes will be extracted whose poems exists
 				if poem_exist is not None:
 					with open("poem_exist.txt", 'a') as file:
 						file.write(poem_name + "~" + author_name + "\n")
@@ -55,7 +62,7 @@ class Get_Poems(scrapy.Spider):
 							line = line.strip()
 							poem += line
 						poem = poem.strip()
-
+						#finally a collection of data is formed which contain author name - date - poem name - poem 
 						if poem != "":
 							with open("collection.txt", 'a') as file:
 								file.write(author_name  + "~"+ self.author_name_to_era[author_name]  + "~" + poem_name + "~" + poem +"~" + "\n")
