a07f08876217ce2f92faf6f10be6d9c73985a5d5
Vishal Singh Rajput <201951171@iiitvadodara.ac.in>
2021-09-23 10:36:12 +0530

    Update merging.py
    added comments and increased readability

--- dataset/merging/merging.py
+++ dataset/merging/merging.py
@@ -1,16 +1,17 @@
+#importing the required libraries
 import pickle
 import csv
-with open ('poems (1).obj','rb') as fin, open ('collection.txt','r') as fin2, open('data_merged.csv','w',newline='',encoding='utf-8') as fout :
-    poems = []
+#opening the required files for merging .obj file contains the data collected through beautiful soap and .txt file contains the data of the poets and poems
+with open ('poems (1).obj','rb') as b_soup, open ('collection.txt','r') as scrapped, open('data_merged.csv','w',newline='',encoding='utf-8') as merger :
+    all_poems = []
     poet_titles = []
     # poets = []
     # titles = []
-
-    for l in fin2:
+#running through each line of the text file and extracting the name of the poet, title of poem and the poem itself
+    for l in scrapped:
 
         line = l.split(sep='~')
-        tem = []
-        
+        temp = []
         poet = line[0].strip()
         year = ''
         title = line[2].strip()
@@ -19,41 +20,44 @@ with open ('poems (1).obj','rb') as fin, open ('collection.txt','r') as fin2, op
         # if poet not in poets:
         #     poets.append(poet)
         
-        tem.append(poet)
+        temp.append(poet)
         poet_titles.append({poet,title})
         # if title not in titles:
         #     titles.append(title)
         
-        for c in line[1]:
-            if c.isnumeric():
+        #here we are extracting the year of the peom
+        for i in line[1]:
+            if i.isnumeric():
                 year += c
         if len(year) > 4:
             year = year[-4:]
-        tem.append(year)
+        temp.append(year)
 
-        tem.append(title)
-
+        temp.append(title)
+#now we try to classify the era of the peom based on the year it was composed
+#if no year is given we do not add the era in the poem 
         if year == '':
-            tem.append('')
+            temp.append('')
         else:
             if int(year) > 1900:
-                tem.append('आधुनिक काल')
+                temp.append('आधुनिक काल')
             elif int(year) > 1700:
-                tem.append('रीति काल')
+                temp.append('रीति काल')
             elif int(year) > 1375:
-                tem.append('भक्ति काल')
+                temp.append('भक्ति काल')
             else:
-                tem.append('आदिकाल')
-
-        tem.append(poem)
-        poems.append(tem)
-        # print(tem)
+                temp.append('आदिकाल')
+# we are storing in each iteration we are storing the poet, year , title, era of poem and th poem itself in a tempp variable
+        temp.append(poem)
+# after storing all the required info in tempp variable we store the poem in poem variable
+        all_poems.append(temp)
+        # print(temp)
         # break
-
-    data = pickle.load(fin)
+#extracting the data in the similar way from the .obj file
+    data = pickle.load(b_soup)
     for line in data:
 
-        tem =[]
+        temp =[]
 
         poet = line[0].strip()
         year = ''
@@ -68,7 +72,7 @@ with open ('poems (1).obj','rb') as fin, open ('collection.txt','r') as fin2, op
 
         
         
-        tem.append(poet)
+        temp.append(poet)
         
         for c in line[1]:
             if c.isnumeric():
@@ -76,27 +80,27 @@ with open ('poems (1).obj','rb') as fin, open ('collection.txt','r') as fin2, op
                 
         if len(year) > 4:
             year = year[-4:]
-        tem.append(year)
+        temp.append(year)
 
-        tem.append(title)
+        temp.append(title)
 
         if year == '':
-            tem.append('')
+            temp.append('')
         else:
             if int(year) > 1900:
-                tem.append('आधुनिक काल')
+                temp.append('आधुनिक काल')
             elif int(year) > 1700:
-                tem.append('रीति काल')
+                temp.append('रीति काल')
             elif int(year) > 1375:
-                tem.append('भक्ति काल')
+                temp.append('भक्ति काल')
             else:
-                tem.append('आदिकाल')
+                temp.append('आदिकाल')
 
-        tem.append(poem)
-        poems.append(tem)
+        temp.append(poem)
+        all_poems.append(temp)
     
-
-    # for a in poems:
-    #     fout.write(str(a) + '\n')
-    writer = csv.writer(fout)
-    writer.writerows(poems)
+#writing all the rows of all_poems in the csv file 
+    # for a in all_poems:
+    #     merger.write(str(a) + '\n')
+    writer = csv.writer(merger)
+    writer.writerows(all_poems)
